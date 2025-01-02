8524b3934fe70d30a35bf7f716f9f2c537c44c4d
arjunss25 <arjunsuresh386@gmail.com>
2025-01-02 23:29:09 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FormValidation from './FormValidation'
+import Formvalthird from './Formvalthird'
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-red-200 flex flex-col items-center justify-center'>
-      <FormValidation/>
+      {/* <FormValidation/> */}
+      <Formvalthird/>
     </div>
   )
 }
